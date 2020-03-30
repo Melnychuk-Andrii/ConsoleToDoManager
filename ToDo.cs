44b0d83162041bc9642aa8fe3d7f6bfac807cb9c
Andrew Melnychuk <androkep@gmail.com>
2020-03-30 17:21:44 +0300

    basic logic added

--- ToDo.cs
+++ ToDo.cs
@@ -4,8 +4,27 @@ namespace ToDoClass
 {
     class ToDo
     {
-        public string Name;
-        public string Description;
-        public Int32 priority;
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Int16 Priority { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool hasDeadline { get; }
+
+        public ToDo(string nm, string dsc, Int16 pr)
+        { Name = nm; Description = dsc; Priority = pr; hasDeadline = false; }
+        public ToDo(string nm, string dsc, Int16 pr, DateTime dt)
+        { Name = nm; Description = dsc; Priority = pr; Deadline = dt; hasDeadline = true; }
+
+        public override string ToString()
+        {
+            string res;
+
+            res = Name + "\tPriority: " + Priority.ToString();
+            if (hasDeadline)
+                res += "\tDeadline: " + Deadline.ToString();
+            res += "\n\n";
+            res += Description;
+            return res;
+        }
     }
 }
--- TodoManager.cs
+++ TodoManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using ToDoClass;
 
@@ -6,9 +7,70 @@ namespace TodoManagerClass
 {
     class TodoManager
     {
+        static List<ToDo> todos = new List<ToDo>();
         static void Main()
         {
-            Console.WriteLine("TodoManager v. 0.01");
+            Console.WriteLine("TodoManager\n" +
+                              "To view the todo list type 'view'\n" +
+                              "To add a new todo type 'add'\n" +
+                              "To leave type 'close'\n");
+            string action = Console.ReadLine();
+
+            while (action != "close")
+            {
+                switch (action)
+                {
+                    case "view": printList(); break;
+                    case "add": addItem(); break;
+                    default: Console.WriteLine("Unknown command!\n" +
+                                               "To view the todo list type 'view'\n" +
+                                               "To add a new todo type 'add'\n" +
+                                               "To leave type 'close'\n");break;
+                }
+                Console.WriteLine("To view the todo list type 'view'\n" +
+                                  "To add a new todo type 'add'\n" +
+                                  "To leave type 'close'\n");
+                action = Console.ReadLine();
+            }
+        }
+
+        static void addItem()
+        {
+            DateTime deadline = new DateTime();
+
+            Console.WriteLine("Type in the name of your new ToDo:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Type in the description of your new ToDo:");
+            string description = Console.ReadLine();
+
+            Console.WriteLine("Type in the priority of your new ToDo(1-10):");
+            Int16 priority;
+            Int16.TryParse(Console.ReadLine(), out priority);
+
+            Console.WriteLine("Does your task have a deadline?(yes/no):");
+            string hasD = Console.ReadLine();
+
+            if (hasD == "yes")
+            {
+                Console.WriteLine("When is the deadline for this ToDo:");
+                DateTime.TryParse(Console.ReadLine(), out deadline);
+            }
+
+            todos.Add((hasD == "yes") ?
+                new ToDo(name, description, priority, deadline) :
+                new ToDo(name, description, priority));
+            Console.WriteLine("ToDo added successfully!");
+        }
+
+        static void printList()
+        {
+            foreach (ToDo todo in todos)
+            {
+                Console.WriteLine("**********************************************************************\n");
+                Console.WriteLine(todo.ToString());
+            }
+            Console.WriteLine("**********************************************************************\n");
         }
     }
 }
